88b75fc24827df0afaaabd006bfbbb6049ff6e29
Burgholzer <Robert.Burgholzer@deq.virginia.gov>
2025-08-15 10:04:13 -0400

    load the grace raster sample

--- R/examples/grace.R
+++ R/examples/grace.R
@@ -1,19 +1,12 @@
 library("terra")
 # grace
-grf <- rast("https://nasagrace.unl.edu/data/20241118/GRACE_GWS_20241118.png")
-GDALinfo(grf)
-terra::describe("https://nasagrace.unl.edu/data/20241118/GRACE_GWS_20241118.png")
-
+# this fails with:
+#    Error in .rasterObjectFromFile(x, band = band, objecttype = "RasterLayer",  : 
+#    Cannot create a RasterLayer object from this file. (file does not exist)
 gr2024 <- raster::raster(
   "https://nasagrace.unl.edu/data/20241118/GRACE_GWS_20241118.png"
 )
-gr2024 <- raster::raster(
-  "https://nasagrace.unl.edu/data/20241118/gws_perc_0125deg_US_20241118.tif"
-)
-gr2024 <- raster::raster(
-  "https://nasagrace.unl.edu/data/20241118/GRACE_GWS_20241118.png"
-)
-# downloaded manually and this works
+# downloaded manually, with a save from web browser and this works
 gr2024 <- raster::raster(
   "C:/Workspace/tmp/GRACE_GWS_20241118.png"
 )
