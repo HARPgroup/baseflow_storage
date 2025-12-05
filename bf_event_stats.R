ff6a14ad7b611ed213b76c7e75a7f27bc8706f75
Ilona Hughes <ilonah22@vt.edu>
2025-12-05 15:34:47 -0500

    Created function to calculate AGWRC and R_squared only

---
+++ bf_event_stats.R
@@ -0,0 +1,13 @@
+bf_event_stats <- function(data, flow_col="Flow", date_col="Date"){
+
+  # Create lm of data
+  logFlow_lm <-lm(log(data[[flow_col]]) ~ data[[date_col]])
+  event_sum <- summary(logFlow_lm)
+  
+  # Assign AWGRC and R-squared
+  AGWRC <- exp(event_sum$coefficients[[2,1]])
+  R_squared <- event_sum$r.squared
+  
+  return(list(AGWRC = AGWRC, R_squared = R_squared))
+  
+}
