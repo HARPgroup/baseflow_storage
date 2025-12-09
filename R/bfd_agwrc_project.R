c6008b75cb3712497b571d109d9d8b6ca5bbc1f8
benhdye <benjamindye@vt.edu>
2025-12-09 12:03:22 -0500

    csvs and trimming script for merge

--- R/bfd_agwrc_project.R
+++
@@ -1,8 +0,0 @@
-bfd_agwrc_project <- function (Q0, C, n) {
-  # Q(t) = Q0 * e^(-k * t)
-  # where t = number of days into the future
-  # k = -ln(C); where C = AGWRC
-  k = -log(C)
-  Qt = Q0 * exp(-k * n)
-  return(Qt)
-}
