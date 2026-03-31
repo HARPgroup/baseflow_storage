6995d44a28789212ec81a889a1b14ed49dd4dac2
Ilona Hughes <ilonah22@vt.edu>
2026-03-30 21:54:11 -0400

    changed site number for area in sqmi

--- convert.flow.R
+++ convert.flow.R
@@ -1,15 +1,10 @@
 # Function to convert flow from cfs to inches using UsGS gage area
-convert.flow <- function(flow_col, gageid){
-  require(dataRetrieval)
+convert.flow <- function(flow_col, area_sqmi){
   cfs <- flow_col
-  
-  # Get drainage area from USGS
-  site <- readNWISsite(paste0(gageid))
-  area <- site$drain_area_va
   
   # Create conversion factor
   conversion <- (86400*12)/(5280*5280)
-  sp_conv <- conversion/area
+  sp_conv <- conversion/area_sqmi
   
   flow_in <- cfs * sp_conv
   
