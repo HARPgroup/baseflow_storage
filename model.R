61008bff922f25f69eaa18d756d44497c7399284
Ilona Hughes <ilonah22@vt.edu>
2026-04-23 09:55:07 -0400

    Separated model and plots from model

--- model.R
+++ model.R
@@ -1,20 +1,4 @@
-# data needed:
-#   - input agws with timestep info
-#     - one agws input?
-#   - AGWO data to match agws
-#     - if only using one agws input, do you only need one agwo?
-#   - Regression coefficients to describe rel between agwrc and q
-#
-# Variables:
-#   - AGWS = input, f(agwo, agwrc)
-#   - AGWO = from model? input data? f(q)?
-#     - AGWO = (1-agwrc)*agws
-#   - AGWRC = f(agws)
-#   - Q = f(agws,agwrc)
-#
-#
-
-
+# Load in data and functions
 event_data <- read.csv("https://raw.githubusercontent.com/HARPgroup/baseflow_storage/refs/heads/main/bf_events_01633000.csv")
 
 source("https://raw.githubusercontent.com/HARPgroup/baseflow_storage/refs/heads/main/add_model_data.R")
@@ -24,12 +8,102 @@ source("https://raw.githubusercontent.com/HARPgroup/baseflow_storage/refs/heads/
 
 
 # Add storage data from model and convert flow to inches
-event_df <- add_model_data(event_data,land_type_code = "forN51171", "AGWS", scenario = "subsheds2",site = "https://deq1.bse.vt.edu:443")
+event_df <- add_model_data(event_data,land_type_code = "forN51171", "AGWS", scenario = "subsheds2",site = "http://deq1.bse.vt.edu:81")
 da_sqmi <- get_drainage_area_sqmi("01633000")
 event_df$Flow_in <- convert.flow(event_df$Flow, da_sqmi)
 
-# Pick example event
-ex <- sqldf("select * from event_df where GroupID=55")
+# optional For loop to do every event ----
+group_ids <- unique(event_df$GroupID)
+all_events <- list()
+
+for (gid in unique(event_df$GroupID)) {
+  
+  ex <- sqldf(sprintf("select * from event_df where GroupID = %d", gid))
+  ex$Date <- as.Date(ex$Date)
+  
+  
+  # Get initial storage value from selected event
+  S0 <- ex$AGWS[1]
+  
+  # Use AGWRC found through trimming for unchanged AGWRC calculations
+  U_AGWRC <- ex$AGWRC[1]
+  
+  # Calc projection with unchanged AGWRC
+  for(i in 1:length(ex$Date)) {
+    
+    if(i==1){
+      ex$U_storage[i] <- S0
+    }else{
+      stor <- as.numeric(ex$U_storage[i-1])
+      nxds <- U_AGWRC*stor
+      
+      ex$U_storage[i] <- nxds
+    }
+  }
+  
+  
+  # Using regression and uniroot ----
+  
+  # get m and b for MJ
+  reg_df <- make_event_regression_df(event_df, da_sqmi, flow_col = "Flow")
+  reg_out <- fit_agwrc_regression_in_day(reg_df)
+  
+  # Set m and b from regression
+  b <- reg_out$coefficients[1]
+  m <- reg_out$coefficients[2]
+  
+  # solve function
+  solve_agwrc_log <- function(S, m, b) {
+    
+    g <- function(C) {
+      C - (m * log(S * (1 - C)) + b)
+    }
+    
+    res <- tryCatch(
+      uniroot(g, lower = 0.001, upper = 0.999)$root,
+      error = function(e) NA_real_
+    )
+    
+    return(res)
+  }
+  
+  ex$V_storage <- NA
+  ex$V_AGWRC <- NA
+  ex$V_flow <- NA
+  
+  # set up repeat regression
+  for (i in 1:length(ex$Date)) {
+    # Set n-1 storage
+    if(i==1){
+      ex$V_storage <- S0
+      ex$V_flow <- ex$Flow_in[i]
+      
+    }else{
+      Stg <- ex$V_storage[i-1]
+      Flw <- ex$Flow_in[i-1]
+      
+      # Run uniroot
+      res <- solve_agwrc_log(S=Stg, m, b)
+      
+      # use calcd agwrc to get storage
+      ex$V_storage[i] <- res * Stg
+      ex$V_flow[i] <- res * Flw
+      ex$V_AGWRC[i] <- res
+      
+    }
+  }
+  
+  all_events[[gid]] <- ex
+  
+}
+
+all_df <- do.call(rbind, all_events)
+all_df$Date <- as.Date(all_df$Date)
+# Clip to model data available
+all_df <- subset(all_df, Date >= as.Date("1983-12-31"))
+
+# Using Sample Event----
+ex <- sqldf("select * from event_df where GroupID=96")
 ex$Date <- as.Date(ex$Date)
 
 
@@ -52,12 +126,9 @@ for(i in 1:length(ex$Date)) {
     }
 }
 
-
-# Using regression and uniroot ----
-
 # get m and b for MJ
 
-reg_df <- make_event_regression_df(event_df, da_sqmi, flow_col = "Flow_in")
+reg_df <- make_event_regression_df(event_df, da_sqmi, flow_col = "Flow")
 reg_out <- fit_agwrc_regression_in_day(reg_df)
 
 # Set m and b from regression
@@ -80,48 +151,55 @@ solve_agwrc_log <- function(S, m, b) {
 }
 
 ex$V_storage <- NA
-
-# Cgecking that res is changing
-r_check <- data.frame(
-  i = numeric(length = length(ex$Date)),
-  r = character(length = length(ex$Date))
-)
+ex$V_AGWRC <- NA
+ex$V_flow <- NA
 
 # set up repeat regression
 for (i in 1:length(ex$Date)) {
   # Set n-1 storage
   if(i==1){
     ex$V_storage <- S0
-    
-    r_check$i[i] <- i
-    r_check$r[i] <- NA
+    ex$V_flow <- ex$Flow_in[i]
     
   }else{
     Stg <- ex$V_storage[i-1]
+    Flw <- ex$Flow_in[i-1]
     
     # Run uniroot
     res <- solve_agwrc_log(S=Stg, m, b)
     
-    r_check$i[i] <- i
-    r_check$r[i] <- res
-    
     # use calcd agwrc to get storage
     ex$V_storage[i] <- res * Stg
+    ex$V_flow[i] <- res * Flw
+    ex$V_AGWRC[i] <- res
+    
   }
 }
 
 
-ggplot(ex, mapping = aes(Date))+
-  geom_line(mapping = aes(y=AGWS, color="Model Data"))+
-  geom_line(mapping = aes(y=U_storage, color="Constant AGWRC"))+
-  geom_line(mapping = aes(y=V_storage, color="Variable AGWRC"))+
-  scale_color_manual(
-    name = "Legend", 
-    values = c("Model Data" = "grey25", 
-               "Constant AGWRC" = "dodgerblue2", 
-               "Variable AGWRC" = "firebrick2"))+
-  coord_cartesian(ylim = c(0,S0+0.01))+
-  theme_bw()+
-  ggtitle(paste0("Storage Calculated using different AGWRCs, MJ Event ", ex$GroupID[1]))+
-  theme(plot.title = element_text(hjust = 0.5))
+# Plugging in arbitrary S values----
+solve_agwrc_log <- function(S, m, b) {
+  
+  g <- function(C) {
+    C - (m * log(S * (1 - C)) + b)
+  }
+  
+  res <- tryCatch(
+    uniroot(g, lower = 0.001, upper = 0.999)$root,
+    error = function(e) NA_real_
+  )
+  
+  return(res)
+}
 
+test_df <- data.frame(Storage = seq(0, 10, by = 0.01), AGWRC = NA)
+
+
+# set up repeat regression
+for (i in 1:length(test_df$Storage)) {
+    # Run uniroot
+    res <- solve_agwrc_log(S=test_df$Storage[i], m, b)
+    
+    test_df$AGWRC[i] <- res
+    
+}
---
+++ model_plots.R
@@ -0,0 +1,134 @@
+# Plots from model.R workflow
+
+# Plots for all event data----
+# plot all storage vs agwrc and flow vs agwrc Check limits
+ggplot(data = all_df, mapping = aes(Flow_in, AGWRC))+
+  geom_point(color="dodgerblue2", size = 0.75)+
+  theme_bw()+
+  coord_cartesian(ylim = c(0.85, 1), xlim = c(0,0.08))+
+  xlab("Flow (in)")+
+  ylab("AGWRC")+
+  ggtitle("All MJ Event Observed Flow and Event AGWRC")+
+  theme(plot.title = element_text(hjust = 0.5))
+
+ggplot(data = all_df, mapping = aes(V_flow, V_AGWRC))+
+  geom_point(color="firebrick2", size = 0.75)+
+  theme_bw()+
+  coord_cartesian(ylim = c(0.85, 1), xlim = c(0,0.08))+
+  xlab("Flow (in)")+
+  ylab("AGWRC")+
+  ggtitle("All MJ Calculated Event Flow and Variable AGWRC")+
+  theme(plot.title = element_text(hjust = 0.5))
+
+#obs(model) vs calc storage
+ggplot(data = all_df, mapping = aes(, V_AGWRC))+
+  geom_point(mapping = aes(x = AGWS,color="Model Storage"), size = 0.75, alpha = 0.15)+
+  geom_point(mapping = aes(x = V_storage,color="Calculated Storage"), size = 0.75)+
+  theme_bw()+
+  scale_color_manual(name = "Legend", values = c("Model Storage" = "purple",
+                                                 "Calculated Storage" = "darkorange")) +
+  coord_cartesian(ylim = c(0.85, 1), xlim = c(0,0.8))+
+  xlab("Storage (in)")+
+  ylab("AGWRC")+
+  ggtitle("All MJ Storage and Variable AGWRC")+
+  theme(plot.title = element_text(hjust = 0.5))
+
+# Obs vs calc flow
+ggplot(data = all_df, mapping = aes(, V_AGWRC))+
+  geom_point(mapping = aes(x = Flow_in,color="Obs Flow"), size = 0.75, alpha = 0.25)+
+  geom_point(mapping = aes(x = V_flow,color="Calculated Flow"), size = 0.75, alpha = 0.25)+
+  theme_bw()+
+  scale_color_manual(name = "Legend", values = c("Obs Flow" = "dodgerblue2",
+                                                 "Calculated Flow" = "firebrick2")) +
+  coord_cartesian(ylim = c(0.85, 1), xlim = c(0,0.08))+
+  xlab("Flow (in)")+
+  ylab("AGWRC")+
+  ggtitle("All MJ Flow and Variable AGWRC")+
+  theme(plot.title = element_text(hjust = 0.5))
+
+# model vs calc stoagre
+ggplot(data=all_df, mapping = aes(AGWS, V_storage))+
+  geom_point()+
+  geom_abline(slope = 1, color = "firebrick2", size = 1)+
+  coord_cartesian(ylim = c(0,0.7), xlim = c(0,0.7))+
+  theme_bw()+
+  xlab("Model AGWS (in)")+
+  ylab("Storage calc using uniroot (in)")+
+  ggtitle("Model vs. Calculated Storage at MJ")+
+  theme(plot.title = element_text(hjust = 0.5))
+
+# obs vs calc flow
+ggplot(data=all_df, mapping = aes(Flow_in, V_flow))+
+  geom_point()+
+  geom_abline(slope = 1, color = "dodgerblue2", size = 1)+
+  coord_cartesian(ylim = c(0,0.1), xlim = c(0,0.1))+
+  theme_bw()+
+  xlab("Obs Flow (in/d)")+
+  ylab("Flow calc using uniroot (in/d)")+
+  ggtitle("Obs vs. Calculated Flow at MJ")+
+  theme(plot.title = element_text(hjust = 0.5))
+
+
+# Plots for using sample event----
+# scale fact for second axis
+scale_factor <- S0 + 0.01
+
+# No flow on plot
+ggplot(data = ex, aes(Date)) +
+  geom_line(aes(y = AGWS, color = "Model Data Storage"), size=0.7) +
+  geom_line(aes(y = U_storage, color = "Constant C Storage"), size = 0.7) +
+  geom_line(aes(y = (AGWRC - 0.7)/0.3 * scale_factor, color = "Constant AGWRC"), linetype="dashed", size=0.7)+
+  geom_line(aes(y = V_storage, color = "Variable C Storage"), size=0.7) +
+  geom_line(aes(y = (V_AGWRC - 0.7) / 0.3 * scale_factor,color = "Variable AGWRC"), linetype="dashed", size = 0.7) +
+  scale_color_manual(name = "Legend", values = c("Model Data Storage" = "purple3",
+                                                 "Constant C Storage" = "lightseagreen",
+                                                 "Constant AGWRC" = "lightseagreen",
+                                                 "Variable C Storage" = "gold2",
+                                                 "Variable AGWRC" = "gold2")) +
+  scale_y_continuous(name = "Storage(in)",sec.axis = sec_axis(~ 0.7 + 0.3 * (. / scale_factor),name = "AGWRC")) +
+  coord_cartesian(ylim = c(0, scale_factor)) +
+  theme_bw() +
+  ggtitle(paste0("Storage and AGWRC over time, MJ Event ", ex$GroupID[1])) +
+  theme(plot.title = element_text(hjust = 0.5))
+
+
+
+# With flow on plot
+ggplot(data = ex, aes(Date)) +
+  geom_line(aes(y = AGWS, color = "Model Data"), size=0.7) +
+  geom_line(aes(y = U_storage, color = "Constant C Storage"), size = 0.7) +
+  geom_line(aes(y = (AGWRC - 0.7)/0.3 * scale_factor, color = "Constant AGWRC"), linetype="dashed", size=0.7)+
+  geom_line(aes(y = V_storage, color = "Variable C Storage"), size=0.7) +
+  geom_line(aes(y = V_flow, color = "Variable C Flow"), size = 0.7) +
+  geom_line(aes(y = (V_AGWRC - 0.7) / 0.3 * scale_factor,color = "Variable AGWRC"), linetype="dashed", size = 0.7) +
+  scale_color_manual(name = "Legend", values = c("Model Data" = "purple3",
+                                                 "Constant C Storage" = "lightseagreen",
+                                                 "Constant AGWRC" = "lightseagreen",
+                                                 "Variable C Storage" = "gold2",
+                                                 "Variable AGWRC" = "gold2",
+                                                 "Variable C Flow" = "forestgreen")) +
+  scale_y_continuous(name = "Storage or Flow (in)",sec.axis = sec_axis(~ 0.7 + 0.3 * (. / scale_factor),
+                                                                       name = "AGWRC")) +
+  coord_cartesian(ylim = c(0, scale_factor)) +
+  theme_bw() +
+  ggtitle(paste0("Storage and AGWRC over time, MJ Event ", ex$GroupID[1])) +
+  theme(plot.title = element_text(hjust = 0.5))
+
+
+# Storage vs. AGWRC Plot
+ggplot(data = ex, aes(V_storage, V_AGWRC))+
+  geom_point(color="firebrick2")+
+  theme_bw()+
+  coord_cartesian(ylim = c(0.7,1))+
+  xlab("Storage (in)")+
+  ylab("AGWRC")+
+  ggtitle(paste0("Storage and AGWRC Values for MJ Event ", ex$GroupID[1]))+
+  theme(plot.title = element_text(hjust = 0.5))
+# Arbitrary S Plots----
+
+ggplot(data=test_df, aes(Storage, AGWRC))+
+  geom_point(color="royalblue4", size = 0.75)+
+  ggtitle("Storage Values vs. Calculated AGWRC")+
+  xlab("Storage (in)")+
+  theme_bw()+
+  theme(plot.title = element_text(hjust = 0.5))
