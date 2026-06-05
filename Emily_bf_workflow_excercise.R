fc3856762a03803f0f419ee361c94a7d3f22bd1a
Emily Simmons <emily07@vt.edu>
2026-06-05 10:35:33 -0400

    Saved files

--- Emily_bf_workflow_excercise.R
+++ Emily_bf_workflow_excercise.R
@@ -1,70 +1,76 @@
-#Roanoke River At Roanoke
-gageID <- "01634000"
+library("devtools")
+#Make sure it hasn't been called, but if it has we can unload it
+unloadNamespace('hydrotools')
+#Get the master branch deployment of the package
+devtools::install_github("HARPgroup/hydro-tools")
+
+#Roanoke 
+gageID <- "02055000"
 
 #Step 01_obtain_flow 
 commandArgs <- function(...){
-  c(gageID, "Roanoke.csv")
+  c(gageID, "RoanokeGage.csv")
 }
-source("C:/Users/gcw73279.COV/Desktop/gitBackups/OWS/meta_model/scripts/river/usgsdata.R")
+source("https://raw.githubusercontent.com/HARPgroup/meta_model/refs/heads/main/scripts/river/usgsdata.R")
 
 #Step 01_event_identification
 commandArgs <- function(...){
   c("RoanokeGage.csv", "obs_date", "obs_flow", FALSE, "RoanokeEvent.csv")
 }
-source("C:/Users/gcw73279.COV/Desktop/gitBackups/OWS/meta_model/scripts/usgs/event_identification.R")
+source("https://raw.githubusercontent.com/HARPgroup/meta_model/refs/heads/main/scripts/usgs/event_identification.R")
 
 #Step 02_baseflow_events
 commandArgs <- function(...){
-  c("strasEvent.csv", "obs_date", "obs_flow", "Strasburg", "strasBF.csv", "monitoring_location_id")
+  c("RoanokeEvent.csv", "obs_date", "obs_flow", "Roanoke", "RoanokeBF.csv", "monitoring_location_id")
 }
-source("C:/Users/gcw73279.COV/Desktop/gitBackups/OWS/meta_model/scripts/usgs/baseflow_events.R")
+source("https://raw.githubusercontent.com/HARPgroup/meta_model/refs/heads/main/scripts/usgs/baseflow_events.R")
 
 #Step 03_baseflow_stats
 commandArgs <- function(...){
-  c("strasBF.csv", "for", "strasstats.csv")
+  c("RoanokeBF.csv", "for", "Roanokestats.csv")
 }
-source("C:/Users/gcw73279.COV/Desktop/gitBackups/OWS/meta_model/scripts/usgs/baseflow_stats.R")
+source("https://raw.githubusercontent.com/HARPgroup/meta_model/refs/heads/main/scripts/usgs/baseflow_stats.R")
 
 
 #Step 04_trim_events
 commandArgs <- function(...){
-  c("strasstats.csv", "strasTrim.csv")
+  c("Roanokestats.csv", "RoanokeTrim.csv")
 }
-source("C:/Users/gcw73279.COV/Desktop/gitBackups/OWS/meta_model/scripts/usgs/bf_trimming.R")
+source("https://raw.githubusercontent.com/HARPgroup/meta_model/refs/heads/main/scripts/usgs/bf_trimming.R")
 
 #Step 05_trim_stats
 commandArgs <- function(...){
-  c("strasTrim.csv", "strasTrimStats.csv")
+  c("RoanokeTrim.csv", "RoanokeTrimStats.csv")
 }
-source("C:/Users/gcw73279.COV/Desktop/gitBackups/OWS/meta_model/scripts/usgs/bf_trimming_analysis.R")
+source("https://raw.githubusercontent.com/HARPgroup/meta_model/refs/heads/main/scripts/usgs/bf_trimming_analysis.R")
 
 
 #Step 06_regression_df
 commandArgs <- function(...){
-  c("strasTrimStats.csv", "01634000", "strasSummaryStats.csv")
+  c("RoanokeTrimStats.csv", "02055000", "RoanokeSummaryStats.csv")
 }
-source("C:/Users/gcw73279.COV/Desktop/gitBackups/OWS/meta_model/scripts/usgs/baseflow_regression_df.R")
+source("https://raw.githubusercontent.com/HARPgroup/meta_model/refs/heads/main/scripts/usgs/baseflow_regression_df.R")
 
 #Step 07_AGWRC_Q_lm
 commandArgs <- function(...){
-  c("strasSummaryStats.csv", "01634000", "strasAGWRCRegression.csv")
+  c("RoanokeSummaryStats.csv", "02055000", "RoanokeAGWRCRegression.csv")
 }
-source("C:/Users/gcw73279.COV/Desktop/gitBackups/OWS/meta_model/scripts/usgs/baseflow_regression.R")
+source("https://raw.githubusercontent.com/HARPgroup/meta_model/refs/heads/main/scripts/usgs/baseflow_regression.R")
 
 #Step import -> 01_regression_coeff
 commandArgs <- function(...){
-  c("01634000", "watershed", "usgs_full_drainage", "agwrc-1.0", 'simple_lm', "strasAGWRCRegression.csv")
+  c("02055000", "watershed", "usgs_full_drainage", "agwrc-1.0", 'simple_lm', "RoanokeAGWRCRegression.csv")
 }
-source("C:/Users/gcw73279.COV/Desktop/gitBackups/OWS/meta_model/scripts/usgs/usgs_post_regression.R")
+source("https://raw.githubusercontent.com/HARPgroup/meta_model/refs/heads/main/scripts/usgs/usgs_post_regression.R")
 
 
 
-flow_data <- read.csv("strasGage.csv")
-event_identification <- read.csv("strasEvent.csv")
-baseflow_events_events <- read.csv("strasBF.csv")
-baseflow_stats <- read.csv("strasstats.csv")
-trim_events <- read.csv("strasTrim.csv")
-trim_stats <- read.csv("strasTrimStats.csv")
-regression_df <- read.csv("strasSummaryStats.csv")
-AGWRC_Q_lm <- read.csv("strasAGWRCRegression.csv")
+flow_data <- read.csv("RoanokeGage.csv")
+event_identification <- read.csv("RoanokeEvent.csv")
+baseflow_events_events <- read.csv("RoanokeBF.csv")
+baseflow_stats <- read.csv("Roanokestats.csv")
+trim_events <- read.csv("RoanokeTrim.csv")
+trim_stats <- read.csv("RoanokeTrimStats.csv")
+regression_df <- read.csv("RoanokeSummaryStats.csv")
+AGWRC_Q_lm <- read.csv("RoanokeAGWRCRegression.csv")
 
