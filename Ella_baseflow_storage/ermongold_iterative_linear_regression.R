d2880f0641fe0d39d35dc9a2beb1af720b7b7e62
Ella Mongold <ermongold@vt.com>
2026-06-11 13:52:01 -0400

    6/11/26 iterative lm edits

--- Ella_baseflow_storage/ermongold_iterative_linear_regression.R
+++ Ella_baseflow_storage/ermongold_iterative_linear_regression.R
@@ -6,7 +6,7 @@ library(tidyverse)
 
 # load in the csv file for a gage of interest
 #### need to replace "gage" with name of gage from workflow ###
-gage_usgs_events <- read_csv("gageSummaryStats.csv")
+gage_usgs_events <- read_csv("https://deq1.bse.vt.edu//usgs//agws//baseflow_summary_df_02055000.csv")
 
 # add a new column for log flow
 reg_df <- gage_usgs_events %>%
@@ -28,7 +28,7 @@ highest_flow <- tail(reg_df$median_flow, 1)
 reg_df_list <- list()
 
 # define the intended range size (flow) for iterations
-range_size <- ### define range ####
+range_size <- 167.4 ### define range ####
 
 # define the start and end of the iteration ranges for the loop
 range_start <- lowest_flow
@@ -36,13 +36,19 @@ range_end <- range_start + range_size
 
 # create a while loop that makes a new dataframe of increasing flow value
 # and row length with each iteration
-while(range_end <= highest_flow) {
+while(range_end <= highest_flow) { 
+  # does not let the loop run if the last value in the range is greater than
+  # the highest flow value in the dataframe
   subset_reg_df <- reg_df[
     reg_df$median_flow >= range_start &
       reg_df$median_flow <= range_end,
     ]
+  # creates a new dataframe that has flow values between the lowest flow and
+  # the lowest flow + the range_size
   reg_df_list[[paste0("range_", range_start, "_to_", range_end)]] <- subset_reg_df
+  # names each subdataframe with the range of values it includes
   range_end <- range_end + range_size
+  # for the next iteration, the range doubles from that of the one before
 }
 
 # get the names of the iterated data frames
@@ -58,5 +64,15 @@ lapply(models, summary)
 
 # extract a list of the R-squared values for each dataframe
 rsq <- lapply(models, function(m) summary(m)$r.squared)
+
+# extract the number of datapoints per dataframe
+n_points <- sapply(reg_df_list, nrow)
+
+# create a dataframe that has a column for R-squared and for number of flow data
+iterative_lm_output <- data.frame(
+  R_squared = unlist(rsq),
+  n_data_points = unlist(n_points)
+)
+
 
 
