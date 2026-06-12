098b61c8b8552b04b846401deff6f0340498c442
Connor Brogan <connor.brogan@deq.virginia.gov>
2026-06-12 09:29:31 -0400

    Mods to lookup table

--- AGWS_between_events_estimator.R
+++ AGWS_between_events_estimator.R
@@ -9,6 +9,10 @@ if (length(argst) < 4) {
   message("Use: model_outflow_calculator.R original_model_data_time_series_daily site_name site_no output_file ")
   q()
 }
+# csv1_path <- "https://deq1.bse.vt.edu:444/usgs/agws/baseflow_trimmed_stats_01634000.csv"
+# csv2_path <- "https://deq1.bse.vt.edu:444/usgs/agws/01634000-flow.csv"
+# m <- -0.0145270564970664
+# b <- 1.04829247996453
 csv1_path <- argst[1]
 csv2_path <- argst [2]
 m <- argst[3]
@@ -16,12 +20,13 @@ b <- argst[4]
 output_file <- argst[5]
 
 #load in baseflow events
-csv1 <- read_csv(csv1_path)
+csv1 <- read.csv(csv1_path)
 
 
 #load in full time series
-csv2 <- read_csv(csv2_path) %>%
-  rename(Date = obs_date, Flow = obs_flow)
+csv2 <- read.csv(csv2_path) %>%
+  rename(Date = obs_date, Flow = obs_flow) |> 
+  filter(!is.na(Flow))
 
 #make sure dates line up
 
@@ -63,13 +68,9 @@ da_sqmi <- df_clean$da_sqmi[1]
 
 
 #rebuilding lookup table:
-
-
-Qts <- seq(min(df_clean$flow_in, na.rm=TRUE),
-           max(df_clean$flow_in, na.rm=TRUE),
-           by = 0.001)
-
-
+Qcfs <- seq(min(df_clean$Flow, na.rm=TRUE),
+            max(df_clean$Flow, na.rm=TRUE),
+            by = 0.1)
 site_factors <- function(da_sqmi, flow_vec = Qts, vec_for_reg = NULL, m, b){
   Qin <- convert.flow(flow_vec, da_sqmi)
   
@@ -79,45 +80,29 @@ site_factors <- function(da_sqmi, flow_vec = Qts, vec_for_reg = NULL, m, b){
   
   C <- b + (m * log(vec_for_reg))
   
-    C <- pmin(pmax(C, 0.001), 0.999)
+  C <- pmin(pmax(C, 0.001), 0.999)
   
-  assign("Qin", Qin, envir = .GlobalEnv)
-  assign("C",   C,   envir = .GlobalEnv)
+  return(data.frame(flow_vec, Qin, C))
 }
 
 
-site_factors(da_sqmi, flow_vec = Qts, m = m, b = b)
+lookupdata <- site_factors(da_sqmi, flow_vec = Qcfs, vec_for_reg = Qcfs, m = m, b = b)
 
-S <- Qin / (1 - C)
-
-Svar <- data.frame(
-  Qts = Qts,
-  C = C,
-  Qin = Qin,
-  S = S
-)
+lookupdata$S <- lookupdata$Qin / (1 - lookupdata$C)
 
 
-Svar$dS <- c(Svar$S[-1] / Svar$S[-length(Svar$S)], NA)
+lookupdata$dS <- c(lookupdata$S[-1] / lookupdata$S[-length(lookupdata$S)], NA)
 
-Svar <- subset(Svar, dS > 1 & S > 0) 
-            
+Svar <- lookupdata[lookupdata$dS > 1 & lookupdata$S > 0,]
+
 #Estimating Storage
-
 df_clean$AGWS_est <- approx(
-  Svar$Qts,
+  Svar$Qin,
   Svar$S,
   xout = df_clean$flow_in,
   rule = 2
 )$y
-
-#Fill missing Storage
-
-df_clean$AGWS_final <- ifelse(
-  is.na(df_clean$AGWS),
-  df_clean$AGWS_est,
-  df_clean$AGWS
-)
+            
 
 #Save as .csv files
 write.csv(df_clean, file = output_file,
