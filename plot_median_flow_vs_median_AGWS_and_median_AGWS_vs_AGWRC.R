dd2235d04de6dd503e1843195bf7b1bb8504dd7d
Emily Simmons <emily07@vt.edu>
2026-06-15 13:29:47 -0400

    updated code

--- plot_median_flow_vs_median_AGWS_and_median_AGWS_vs_AGWRC.R
+++ plot_median_flow_vs_median_AGWS_and_median_AGWS_vs_AGWRC.R
@@ -1,36 +1,42 @@
 #Import your gages csv
-#on line 22 change the gage ID to your gage ID
+#on line 28 change the gage ID to your gage ID
 #Change title names on plots to reflect your gage
+library(hydrotools)
+library(dataRetrieval)
+library(tidyverse)
+library(dplyr)
 
 #import the csv
-baseflow_trimmed_stats <- read_csv("C:\\HARP Folder\\baseflow_storage\\Shenandoah_Millville_combined_baseflow_stats.csv")
+baseflow_trimmed_stats <- read_csv("C:\\HARP Folder\\baseflow_storage\\ForecastingApp\\Cootes_combined_baseflow_stats.csv")
 baseflow_trimmed_stats <- baseflow_trimmed_stats
 
 #Create function to find drainage area (sq.mi) and Flow per drainage area (cfs/sq. mi)  
 add_drainage_area <- function(baseflow_trimmed_stats, gageID) {
-  da <- readNWISsite(gageID) %>%
-    pull(drain_area_va)
+  da <- read_waterdata_monitoring_location(
+    monitoring_location_id = paste0("USGS-", gageID)
+  ) %>%
+    pull(drainage_area)
+  
   baseflow_trimmed_stats %>%
     mutate(
-      DrainageArea = da,
+      drainage_area = da,
       Flow_per_DA = Flow / da
     )
 }
-
 baseflow_trimmed_stats <- add_drainage_area(
   baseflow_trimmed_stats,
-  "01636500"
+  "01634000"
 )
 #Function to convert flow from cfs to inches using the USGS gage area
 source("C:\\HARP Folder\\baseflow_storage\\convert.flow.R")
 
 baseflow_trimmed_stats <- baseflow_trimmed_stats %>% 
   mutate(
-    flow_in = convert.flow(Flow, DrainageArea)
+    flow_in = convert.flow(Flow, drainage_area)
   )
 baseflow_trimmed_stats <- baseflow_trimmed_stats %>% 
   mutate(
-    median_flow_in = convert.flow(median_flow, DrainageArea)
+    median_flow_in = convert.flow(median_flow, drainage_area)
   )
 
 
@@ -42,7 +48,7 @@ baseflow_trimmed_stats <- baseflow_trimmed_stats %>%
 ggplot(baseflow_trimmed_stats, aes(x = median_flow_in, y = AGWS)) +
   geom_point(size = 1.5) +
   labs(
-    title = "Median Flow vs. Median AGWS For Shenandoah at Millville (01636500)",
+    title = "Median Flow vs. Median AGWS For Cootes Store (01634000)",
     x = "Median Q (in/day)",
     y = "Median AGWS (WS-in.)"
   ) +
@@ -52,7 +58,7 @@ ggplot(baseflow_trimmed_stats, aes(x = median_flow_in, y = AGWS)) +
 ggplot(baseflow_trimmed_stats, aes(x = AGWS, y = AGWRC)) +
   geom_point(size = 1.5) +
   labs(
-    title = "Median AGWS vs. AGWRC For Shenandoah at Millville (01636500)",
+    title = "Median AGWS vs. AGWRC For Cootes Store (01634000)",
     x = "AGWS (WS-in.)",
     y = "AGWRC"
   ) +
