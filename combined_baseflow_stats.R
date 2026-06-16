d3177e8476da7add11036d66afd4731df4adfb98
Emily Simmons <emily07@vt.edu>
2026-06-16 15:34:13 -0400

    updated 6/16/2026

--- combined_baseflow_stats.R
+++ combined_baseflow_stats.R
@@ -1,16 +1,16 @@
 library(dplyr)
 #reads in the two csv
-baseflow_trimmed_stats <- read_csv("C:\\HARP Folder\\baseflow_storage\\baseflow_trimmed_stats_01636500.csv")
+baseflow_trimmed_stats <- read_csv("C:\\HARP Folder\\baseflow_storage\\baseflow_trimmed_stats_01634000.csv")
 baseflow_trimmed_stats <- baseflow_trimmed_stats
 
-baseflow_summary_stats <-read_csv ("C:\\HARP Folder\\baseflow_storage\\baseflow_summary_df_01636500.csv")
+baseflow_summary_stats <-read_csv ("C:\\HARP Folder\\baseflow_storage\\baseflow_summary_df_01634000.csv")
 baseflow_summary_stats <- baseflow_summary_stats
 
 # selects GroupID and median_flow columns from baseflow_summary_stats
 trimmed_baseflow_summary_stats <- baseflow_summary_stats %>% 
-  select(GroupID, median_flow)
+  select(GroupID, median_flow, event_AGWRC)
 
 # combined all columns into one object 
 combined_baseflow_stats <- merge(baseflow_trimmed_stats, trimmed_baseflow_summary_stats, by = "GroupID", all.x = TRUE)
 
-write.csv(combined_baseflow_stats, "Shenandoah_Millville_combined_baseflow_stats.csv", row.names = FALSE)
+write.csv(combined_baseflow_stats, "Cootes_combined_baseflow_stats.csv", row.names = FALSE)
