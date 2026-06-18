c70b89e49bc2d841515acf277fc994877d5b0e6f
willprokopik <willprokopik@vt.edu>
2026-06-18 09:14:10 -0400

    WaterGageBase

--- ForecastingApp/server.R
+++ ForecastingApp/server.R
@@ -5,7 +5,7 @@ server <- function(input, output, session) {
   gage_obj <- reactiveVal(NULL)
   observe({
     gage_id <- gsub("^([0-9]+) -.*","\\1",input$site_choice)
-    gage <- hydrotools::WaterGageDaily$new(
+    gage <- hydrotools::WaterGageBase$new(
       # ds_in = ds,
       gage_id = gage_id
     )
