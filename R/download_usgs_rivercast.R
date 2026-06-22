defc747375bd56801a86d5303b94f51220c1c3b0
Connor Brogan <connor.brogan@deq.virginia.gov>
2026-06-22 16:25:15 -0400

    Download rivercast

--- R/download_usgs_rivercast.R
+++ R/download_usgs_rivercast.R
@@ -1,7 +1,7 @@
 
 args <- commandArgs(trailingOnly = TRUE)
 pathout <- args[1]
-# pathout <- "C:/Users/gcw73279.COV/Downloads/"
+# pathout <- "C:/Users/gcw73279.COV/Desktop/gitBackups/gitHome/rivercast/"
 # Automatically Download USGS Drought Forecast
 
 # install.packages("chromote")
@@ -22,24 +22,30 @@ session$Browser$setDownloadBehavior(
 
 # Opens website
 session$Page$navigate("https://water.usgs.gov/vizlab/streamflow-drought-forecasts/?extent=Virginia#6.7/38.018/-79.459")
+Sys.sleep(3) 
 
 # Bypass 'Get Started' button
 session$Runtime$evaluate(expression = "document.querySelector('#access-button').click()")
+Sys.sleep(3) 
 
 # Downloads USGS Forecast PARQUET file
 session$Runtime$evaluate(expression = "document.querySelector('#download-forecasts > span').click()")
 
 # Allows download to finish before closing
+csv_name <- paste0(pathout,"USGS_rc_", Sys.Date() - 1, ".csv")
+message("Download folder is ",download_folder)
+message("Writing to ",csv_name)
+Sys.sleep(5)
 session$close()
 
 ### Sort files to get most recent file
 files <- list.files(path = download_folder, pattern = "\\.parquet$", full.names = TRUE)
 most_recent_file <- files[which.max(file.info(files)$mtime)]
 
+dataIn <- read_parquet(most_recent_file)
+message("Found ",nrow(dataIn), " rows")
+Sys.sleep(5)
 # Write csv
-csv_name <- paste0(pathout,"USGS_rc_", Sys.Date() - 1, ".csv")
-
-read_parquet(most_recent_file) |> 
-  write_csv_arrow(csv_name)
+write_csv_arrow(dataIn, csv_name)
 
 # push csv to github
