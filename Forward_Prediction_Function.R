dc200ab7aaa84466d191dab90acc36575e28e4c7
Emily Simmons <emily07@vt.edu>
2026-06-23 08:41:51 -0400

    updated 6/23/26

--- Forward_Prediction_Function.R
+++ Forward_Prediction_Function.R
@@ -1,12 +1,13 @@
+#Forward calculation Prediction, with an unknown AGWRC value 
 library(tidyverse)
 library(patchwork)
 library(gt)
 
 source("https://raw.githubusercontent.com/HARPgroup/baseflow_storage/refs/heads/main/FinalRegression.R")
 
-flow_csv <- read_csv("https://deq1.bse.vt.edu/usgs/agws/02056000-flow.csv")
-baseflow_csv <- read_csv("https://deq1.bse.vt.edu/usgs/agws/baseflow_trimmed_stats_02056000.csv")
-baseflow_final <- read_csv("https://deq1.bse.vt.edu/usgs/agws/baseflow_summary_df_02056000.csv")
+flow_csv <- read_csv("https://deq1.bse.vt.edu/usgs/agws/01634000-flow.csv")
+baseflow_csv <- read_csv("https://deq1.bse.vt.edu/usgs/agws/baseflow_trimmed_stats_01634000.csv")
+baseflow_final <- read_csv("https://deq1.bse.vt.edu/usgs/agws/baseflow_summary_df_01634000.csv")
 
 model <- fit_agwrc_regression(baseflow_final)
 
@@ -42,29 +43,59 @@ for (d in days) {
   FlowResults[[paste0("Bias_", d, "day")]] <- mean(FlowResults[[paste0("proj_", d, "day")]] - FlowResults[[paste0("observ_", d, "day")]])
   FlowResults[[paste0("r.squared_", d, "day")]] <- summary(lm(as.formula(paste0("proj_", d, "day", "~", "observ_", d, "day")), data = FlowResults))$r.squared
   FlowResults[[paste0("residuals_", d, "day")]] <- FlowResults[[paste0("proj_", d, "day")]] - FlowResults[[paste0("observ_", d, "day")]]
+  
+  threshold <- quantile(abs(FlowResults[[paste0("residuals_", d, "day")]]),probs = 0.90,na.rm = TRUE)
+  FlowResults[[paste0("flag_", d, "day")]] <-abs(FlowResults[[paste0("residuals_", d, "day")]]) > threshold
+  
+  cat("\n", d, "day threshold =", threshold, "\n")
+  FlowResults %>%
+    filter(abs(.data[[paste0("residuals_", d, "day")]]) > threshold) %>%
+    select(
+      Date,
+      all_of(paste0("residuals_", d, "day")),
+      all_of(paste0("flag_", d, "day"))
+    ) %>%
+    print()
+  
   # Accuracy table with x columns for all day ranges predicted
   accuracy_table[[paste0("FlowStats_", d, "day")]] <- c(FlowResults[[paste0("MAE_", d, "day")]][1], FlowResults[[paste0("RMSE_", d, "day")]][1], FlowResults[[paste0("MAPE_", d, "day")]][1], FlowResults[[paste0("Bias_", d, "day")]][1],  FlowResults[[paste0("r.squared_", d, "day")]][1])
 }
+# Plot 1, scatter plot of Residuals and baseline y = 0
+  ggplot(FlowResults, aes(Date, residuals_7day)) +
+    geom_hline(yintercept = 0, color = "black") +
+    geom_point(color = "purple", size = 1.3) +
+    geom_point(
+      data = filter(FlowResults, flag_7day),
+      color = "red",
+      size = 1.3) +
+    labs(title = "Projected vs. Observed Residuals (7- Day Period)")+
+    theme_classic()
+  
+  # Plot 2, scatter plot of Residuals and baseline y = 0
+  ggplot(FlowResults, aes(Date, residuals_15day)) +
+    geom_hline(yintercept = 0, color = "black") +
+    geom_point(color = "purple", size = 1.3) +
+    geom_point(
+      data = filter(FlowResults, flag_15day),
+      color = "red",
+      size = 1.3) +
+    labs(title = "Projected vs. Observed Residuals (15- Day Period)")+
+    theme_classic()
+  
+  
+######################################################################## 
+  # Plot 1, scatter plot of Actual vs. Predicted
+  ggplot(FlowResults, aes(x = Date)) +
+    geom_point(aes(y = observ_7day, color = "Observed"), size = 1.3) +
+    geom_point(aes(y = proj_7day, color = "Projected"), size = 1.3) +
+    labs(title = "Projected vs. Observed Flow in Baseflow Events (7-Day Period)", y = "Flow (cfs)", x = "Date", color = "Flow")+
+    theme_classic()
 
   ggplot(FlowResults, aes(x = proj_7day, y = observ_7day)) +
-  geom_point(color = "blue", size = 1.3) +
-  geom_smooth(method = "lm", se = F, color = "red") +
-  labs(title = "Projected vs. Observed Flow in Baseflow Events (7- day Period)", y = "Observed Flow (cfs)", x = "Projected Flow (cfs)") +
-  theme_classic()
-
-# Plot 1, scatter plot of Actual vs. Predicted
-  ggplot(FlowResults, aes(x = Date)) +
-  geom_point(aes(y = observ_7day, color = "Observed"), size = 1.3) +
-  geom_point(aes(y = proj_7day, color = "Projected"), size = 1.3) +
-  labs(title = "Projected vs. Observed Flow in Baseflow Events (7-Day Period)", y = "Flow (cfs)", x = "Date", color = "Flow")+
-  theme_classic()
-
-# Plot 2, scatter plot of Residuals and baseline y = 0
-  ggplot(FlowResults, aes(Date, residuals_7day)) +
-  geom_hline(yintercept = 0, color ="black") +
-  geom_point(color = "purple", size = 1.3) +
-  labs(title = "Projected vs. Observed Residuals (7-Day Period)", x = "Date", y = "Projected vs. Observed (cfs)") +
-  theme_classic()
+    geom_point(color = "blue", size = 1.3) +
+    geom_smooth(method = "lm", se = F, color = "red") +
+    labs(title = "Projected vs. Observed Flow in Baseflow Events (7- day Period)", y = "Observed Flow (cfs)", x = "Projected Flow (cfs)") +
+    theme_classic()
   
   accuracy_table |>
     gt() |>
@@ -72,3 +103,45 @@ for (d in days) {
   
 # Plot 1 and Plot 2
 #p1/p2
+#########################################################  
+  # threshold <- quantile(
+  #   abs(FlowResults$residuals_7day),
+  #   probs = 0.95,
+  #   na.rm = TRUE
+  # )
+  # 
+  # FlowResults <- FlowResults %>%
+  #   mutate(
+  #     flag_7day = abs(residuals_7day) > threshold
+  #   )
+  
+############################################################  
+  # threshold <- quantile(
+  #   abs(FlowResults$residuals_15day),
+  #   probs = 0.95,
+  #   na.rm = TRUE
+  # )
+  # 
+  # FlowResults <- FlowResults %>%
+  #   mutate(
+  #     flag_15day = abs(residuals_15day) > threshold
+  #   ) 
+  
+#####################################################
+  # FlowResults <- FlowResults %>%
+  #   mutate(
+  #     pct_error_15day =
+  #       100 * abs(proj_15day - observ_15day) / observ_15day
+  #   )
+  # 
+  # threshold <- quantile(
+  #   FlowResults$pct_error_15day,
+  #   0.95,
+  #   na.rm = TRUE
+  # )
+  # 
+  # FlowResults <- FlowResults %>%
+  #   mutate(
+  #     flag_15day = pct_error_15day > threshold
+  #   )
+  
