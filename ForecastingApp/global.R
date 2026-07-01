dcee98aefe22de7fcc811bfe246bda7da35b55ef
Connor Brogan <connor.brogan@deq.virginia.gov>
2026-07-01 16:24:02 -0400

    Updated package to remove hydrotools dependency and to update buildignore

--- ForecastingApp/global.R
+++ ForecastingApp/global.R
@@ -14,6 +14,7 @@ suppressPackageStartupMessages({
   library(dataRetrieval)
   library(httr)
   library(sqldf)
+  library(shinybusy)
   #For getting data from R server
 #  library(DEQmethods)
   library(pins)
