9b04714458f937dbb14096b3e1e9a6897d64682e
benhdye <benjamindye@vt.edu>
2026-07-21 11:44:12 -0400

    fixed roxygen headers lookup

--- R/build_lookup.R
+++ R/build_lookup.R
@@ -1,12 +1,12 @@
 #'@title site_factors
 #'@name site_factors
-#' @details A function...
-#' @param da_sqmi numeric
-#' @param flow_vec numeric
-#' @param vec_for_reg numeric
-#' @param m numeric
-#' @param b numeric
-#' @return A data frame...
+#' @details A function that prepares data to be turned into a lookup table
+#' @param da_sqmi numeric value of the drainage area in square miles for a gage
+#' @param flow_vec numeric flow vector for a gage
+#' @param vec_for_reg numeric vector from gage data that will be regressed
+#' @param m numeric slope value from the site
+#' @param b numeric intercerpt value from site
+#' @return A data frame of data that has a C, m, b, drainage area, flow vector, and a vector for regression that is ready a lookup table can be built from
 #' @export
 site_factors <- function(da_sqmi, flow_vec, vec_for_reg = NULL, m, b){
   #convert flow fron cfs to watershed inches per day
@@ -27,20 +27,17 @@ site_factors <- function(da_sqmi, flow_vec, vec_for_reg = NULL, m, b){
 
 #'@title build_lookup
 #'@name build_lookup
-#' @details A function...
-#' @param df_clean data frame
-#' @param da_sqmi numeric
-#' @param m numeric
-#' @param b numeric
-#' @param n_steps numeric
-#' @param vec_for_reg numeric
-#' @return A data frame...
+#' @details A function that takes a data frame of C, m, b, and flows that can create a lookup table based on these values
+#' @param df_clean cleaned data frame that is ready to be turned into a lookup table
+#' @param da_sqmi numeric value of the drainage area for the gage in square miles
+#' @param m numeric slope value for the gage
+#' @param b numeric intercept value for the gage
+#' @param n_steps numeric number of steps required in the lookup table, ex. 25 is 25 steps, more is more precise but has a longer compute time
+#' @param vec_for_reg numeric vector that had the regression applied to it
+#' @return A data frame containing a lookup table based on C, m, b, and flow values that can be used to predict variables when missing some but having others 
 #' @export
-build_lookup <- function(df_clean, da_sqmi, m, b, n_steps = 25, vec_for_reg = NULL) {
-
+build_lookup <- function(obs_flow, da_sqmi, m, b, n_steps = 25, vec_for_reg = NULL) {
   #only baseflow periods
-  obs_flow <- df_clean$Flow[!is.na(df_clean$AGWRC)]
-
   min_flow <- min(obs_flow, na.rm = TRUE)
   max_flow <- max(obs_flow, na.rm = TRUE)
 
