3205fae8b855e9e5dc3caa8125e24a8865347c38
benhdye <benjamindye@vt.edu>
2026-07-22 11:23:22 -0400

    weighted R-squared

--- workspace/benjamin_dye/lm_trimming_function.R
+++ workspace/benjamin_dye/lm_trimming_function.R
@@ -1,4 +1,4 @@
-window_lm_table <- function(event, min_win = 3) {
+window_lm_table <- function(event, min_win = NULL, weight_power = 1) {
   
   event <- event %>%
     mutate(
@@ -8,6 +8,11 @@ window_lm_table <- function(event, min_win = 3) {
     arrange(Date)
   
   n <- nrow(event)
+  
+  # Default min_win to half the event length
+  if (is.null(min_win)) {
+    min_win <- ceiling(n / 2)
+  }
   
   results <- list()
   k <- 1
@@ -22,19 +27,26 @@ window_lm_table <- function(event, min_win = 3) {
       
       fit <- lm(AGWR ~ Day, data = window)
       s <- summary(fit)
+      r2 <- s$r.squared
+      
+      #Length-weighted R2
+      
+      length_weight <- (win_len / n) ^ weight_power
+      weighted_r2 <- r2 * length_weight
       
       results[[k]] <- data.frame(
-        i          = i,
-        j          = j,
-        win_len    = win_len,
-        date_start = min(window$Date),
-        date_end   = max(window$Date),
-        slope      = coef(fit)[2],
-        intercept  = coef(fit)[1],
-        r2         = s$r.squared,
-        adj_r2     = s$adj.r.squared,
-        rmse       = sqrt(mean(residuals(fit)^2)),
-        p_value    = coef(s)[2, 4]
+        i             = i,
+        j             = j,
+        win_len       = win_len,
+        date_start    = min(window$Date),
+        date_end      = max(window$Date),
+        slope         = coef(fit)[2],
+        intercept     = coef(fit)[1],
+        r2            = r2,
+        adj_r2        = s$adj.r.squared,
+        weighted_r2   = weighted_r2,
+        rmse          = sqrt(mean(residuals(fit)^2)),
+        p_value       = coef(s)[2, 4]
       )
       
       k <- k + 1
