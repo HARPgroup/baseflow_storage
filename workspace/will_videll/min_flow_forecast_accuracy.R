fd189a740998d13f31550f38a2170e245de28c09
wavidell <wavidell@vt.edu>
2026-07-23 15:01:06 -0400

    updated forecast accuacy function

--- workspace/will_videll/min_flow_forecast_accuracy.R
+++ workspace/will_videll/min_flow_forecast_accuracy.R
@@ -1,11 +1,11 @@
-#basepath <- "/var/www/R"
-#source("/var/www/R/config.R")
-#library(hydrotools)
-#library(agws)
-#library(tidyverse)
-#gage_obj <- WaterGageDaily$new(gage_id = "03524000", ds_in = ds)
-#event_df <- read_csv("C:/HARP/baseflow_storage/step2_03524000.csv")
-#var <- gage_obj$baseflow_workflow_data(omsite)
+basepath <- "/var/www/R"
+source("/var/www/R/config.R")
+library(hydrotools)
+library(agws)
+library(tidyverse)
+gage_obj <- WaterGageDaily$new(gage_id = "03524000", ds_in = ds)
+var <- gage_obj$baseflow_workflow_data(omsite)
+event_df <- var$trimmed_events_df
 #' @title min_flow_accuracy
 #' @name min_flow_accuracy
 #' @description
@@ -19,39 +19,81 @@
 #'
 #' @returns df with GroupID, start_date, obs_flow, proj_flow, abs_err, abs_pcnt_err.
 #' @export min_flow_accuracy
-min_flow_accuracy <- function(gage_obj, event_df){
+#' 
+min_flow_accuracy <- function(gage_obj, event_df, AGWRC = c("lm_constant", "lm_variable")){
   
   #group event data by GroupID, 1 row per event.Create empty columns for data.
   event_df1 <- event_df |> 
     dplyr::group_by(GroupID) |> 
     dplyr::summarize(start_date = min(Date),
-                     obs_min_flow = NA,
-                     proj_flow = NA,
-                     min_flow_date = NA)
+                     local_min1_date = NA,
+                     days_overestimated = NA,
+                     trough_cnt = NA)
   
   #loop through each event
   for(i in 1:nrow(event_df1)){
     
     #run agws::forwardForecast() at event start date and slice the day with the lowest observed flow.
-    forecast <- gage_obj$baseflow_forecast(start_date = event_df1$start_date[i], AGWRC = "lm_variable",
-                                           use_limits = TRUE) |> 
-      dplyr::slice_min(obs_flow)
+    forecast <- gage_obj$baseflow_forecast(start_date = event_df1$start_date[i], AGWRC = AGWRC,
+                                           use_limits = TRUE) 
+    
+    forecast$overestimate[forecast$Forecast > forecast$obs_flow] <- TRUE
+    forecast$trough <- rollapply(forecast$obs_flow, width = 7, function(x) x[4] <= min(x[-4]), fill = FALSE)
+    
+    troughs <- forecast |> 
+      dplyr::filter(trough) |> 
+      group_by(run_id = consecutive_id(obs_flow)) |> 
+      slice_tail(n = 1) |> 
+      ungroup() |> 
+      select(Date, trough)
+    
+    forecast$trough <- FALSE # reset trough column
+    forecast <- forecast |> 
+      rows_update(troughs, by = "Date") # update trough column with filtered troughs
+    
+    forecast$upper_bound <- forecast$obs_flow * 1.10
+    forecast$lower_bound <- forecast$obs_flow * .90
+    forecast <- forecast |> 
+      mutate(is_accurate = trough * (Forecast > lower_bound) & (Forecast < upper_bound))
+    
+    troughs <- forecast |> 
+      filter(trough) 
+    longest_streak <- with(rle(troughs$is_accurate), if(any(values)) max(lengths[values]) else 0)
     
     #fill empty data columns with values from sliced forecast
-    event_df1$obs_min_flow[i] <- forecast$obs_flow[1]
-    event_df1$proj_flow[i] <- forecast$Forecast[1]
-    event_df1$min_flow_date[i] <- forecast$Date[1]
-    event_df1$min_flow_date <- as.Date(event_df1$min_flow_date)
+    event_df1$local_min1_date[i] <- troughs$Date[1]
+    event_df1$local_min1_date <- as.Date(event_df1$local_min1_date)
+    event_df1$days_overestimated[i] <- sum(forecast$overestimate, na.rm = TRUE)
+    event_df1$trough_cnt[i] <- sum(forecast$trough)
+    event_df1$accurate_troughs[i] <- sum(forecast$is_accurate)
+    event_df1$longest_acc_streak[i] <- longest_streak
   }
   
-  #calculate error
-  event_df1 <- event_df1 |> 
-    dplyr::mutate(days_after_start = min_flow_date - start_date,
-                  abs_err = abs(proj_flow - obs_min_flow),
-                  abs_pcnt_err = abs(abs_err/obs_min_flow) * 100)
   return(event_df1)
 }
 
-#test <- min_flow_accuracy(gage_obj, event_df)
+test <- min_flow_accuracy(gage_obj, event_df, AGWRC = "lm_variable")
 
-troughs <- rollapply(df$y, width = 7, function(x) x[4] < min(x[-4]), fill = FALSE)
+
+## Make this a function
+
+plot_event_minima <- function(gage_obj, start_date){
+  forecast1 <- gage_obj$baseflow_forecast(start_date = start_date, AGWRC = "lm_variable",
+                                       use_limits = TRUE)
+  forecast1$troughs <- rollapply(forecast1$obs_flow, width = 7, function(x) x[4] <= min(x[-4]), fill = FALSE)  
+  
+  forecast_trimmed <- forecast1 |> 
+    dplyr::filter(troughs) |> 
+    group_by(run_id = consecutive_id(obs_flow)) |> 
+    slice_tail(n = 1) |> 
+    ungroup() 
+  
+  plot <- gage_obj$plot_baseflow_forecast(start_date = start_date, include_days_before = 60)+
+   geom_point(aes(x = forecast_trimmed$Date, y = forecast_trimmed$obs_flow)) 
+ 
+  return(plot)
+}
+
+start_date <- "1936-05-03"
+plot_event_minima(gage_obj, start_date)
+
